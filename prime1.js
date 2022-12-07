0c98a2e24fc754d389a25c4f3450666eccdda892
prathamesh magar <prathameshma1@gmail.com>
2022-12-07 15:23:24 +0530

    I correct my code

--- prime1.js
+++ prime1.js
@@ -1,10 +1,10 @@
-//function identifyPrime(num) {//
+function identifyPrime(num) {
     let count=0;
     for(let i=0;i<=num;i++){
         if(num%i===0){
             count++;
         }
-    }if(count%2){
+    }if(count==2){
             console.log("Yes")
         }else{
             console.log("No")
